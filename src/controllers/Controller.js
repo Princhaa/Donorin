6eab0f22dfa32a787fe67270f463dacbd17a747f
Ananta Pratama <deras.hujan15@gmail.com>
2017-12-06 10:15:18 +0700

    Fix code formatting

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,9 +1,9 @@
 import ApiRequest from '../service/APIRequest/ApiRequest'
 
 class Controller {
-  get api() {
-    return ApiRequest;
-  }
+	get api() {
+		return ApiRequest
+	}
 }
 
 export default Controller
